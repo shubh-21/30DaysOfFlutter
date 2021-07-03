bdd19a2aafcaf540d9f5fc4cb47a5e676184f830
shubh-21 <shubhamacharekar18@gmail.com>
2021-07-03 20:54:25 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+    final int days=30;
+    final String name = "shubham";
+    
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Material(
+        child: Center(
+          child: Container(
+            child:Text("Welcome to $days days of flutter by $name") ,
+            ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
